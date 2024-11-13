834fea124854469152fa1a8f11c72d56c6118a70
Naimat Ullah Khan <naimatullahkhan@Naimats-MacBook-Pro.local>
2024-11-13 15:31:49 +0400

    Added code

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, Vite + React!</h1>
+      <p>Welcome to your Vite-powered React app.</p>
+    </div>
+  );
+}
+
+export default App;
